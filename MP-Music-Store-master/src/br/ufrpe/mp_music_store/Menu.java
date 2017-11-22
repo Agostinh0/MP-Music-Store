227d6b29918b4903c5433fbedede8f67d13591de
Perior <theperior@hotmail.com>
2017-11-22 00:30:42 -0200

    ConstrutoresBase
    Basicão dos contrutores

---
+++ MP-Music-Store-master/src/br/ufrpe/mp_music_store/Menu.java
@@ -0,0 +1,14 @@
+package br.ufrpe.mp_music_store;
+import java.util.Scanner;
+
+import br.ufrpe.mp_music_store.negocio.beans.Cd;
+
+public class Menu {
+	
+	public static void main(String[] args){
+		
+		Cd kek = new Cd("Cast In Steel", 2015 , "a-ha", 22);
+		
+		System.out.println(kek.toString());
+	}
+}
---
+++ MP-Music-Store-master/src/br/ufrpe/mp_music_store/negocio/CatalogoCds.java
@@ -0,0 +1,47 @@
+package br.ufrpe.mp_music_store.negocio;
+
+import br.ufrpe.mp_music_store.dados.RepositorioCds;
+import br.ufrpe.mp_music_store.negocio.beans.Cd;
+
+public class CatalogoCds {
+	private RepositorioCds repositorio;
+	private static CatalogoCds instance;
+	
+	private CatalogoCds() {
+		this.repositorio = new RepositorioCds(10);
+	}
+	
+	
+	public void adicionaCds(Cd c) {
+		if(c == null) {
+			//error message
+		}
+		else {
+			repositorio.cadastrar(c);
+		}
+	}
+	
+	public Cd buscarCds(String nome) {
+		if(nome == null) {
+			//return error message
+		}
+		return this.repositorio.procurar(nome);
+	}
+	
+	public void removerCds(String nome) {
+		if(nome == null) {
+			//message error
+		}
+		else {
+			repositorio.remover(nome);
+		}
+	}
+	
+	public boolean existeCd(String titulo) {
+		if(titulo == null) {
+			//return fudeu amigo
+		}
+		
+		return this.repositorio.existe(titulo);
+	}
+}
